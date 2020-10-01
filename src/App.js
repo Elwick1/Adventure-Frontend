ba5c8c4ac13d0b76ffdce25bb31ae65c3049d76a
Elwick1 <michaelrwilliamson92@gmail.com>
2020-10-01 17:43:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
-function App() {
+export default class App extends React.Component {
+
+  render () {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Michael!
         </p>
         <a
           className="App-link"
@@ -22,5 +29,13 @@ function App() {
     </div>
   );
 }
+}
 
-export default App;
+// This will be the home page of the app
+// Within the homepage will feature ;
+// - the game credits
+// - rules/game information
+// - Game launch
+//  STRETCH GOALS
+// - Highscore Board
+
